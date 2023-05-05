fba5156e4c0d32408a08656c7e33ba5cd226794d
Zebin YANG <yangzb2010@connect.hku.hk>
2023-05-05 14:44:34 +0800

    Modify data prepare

--- docs/_build/html/_downloads/ba163cdea28be131742a4208b4eb7cde/plot_3_data_prepare.py
+++ docs/_build/html/_downloads/ba163cdea28be131742a4208b4eb7cde/plot_3_data_prepare.py
@@ -15,3 +15,8 @@ exp.data_loader(data="BikeSharing", silent=True)
 
 exp.data_prepare(target='cnt', task_type='Regression', sample_weight=None,
                        split_method='random', test_ratio=0.2, random_state=0)
+
+import numpy as np
+custom_train_idx = np.arange(0,16000)
+custom_test_idx = np.arange(16000, 17379)
+exp.data_prepare(train_idx=custom_train_idx, test_idx=custom_test_idx)
--- docs/_build/jupyter_execute/guides/data/data_prepare.py
+++ docs/_build/jupyter_execute/guides/data/data_prepare.py
@@ -4,6 +4,14 @@
 # In[ ]:
 
 
+custom_train_idx = np.arange(0,16000)
+custom_test_idx = np.arange(16000, 17379)
+exp.data_prepare(train_idx=custom_train_idx, test_idx=custom_test_idx)
+
+
+# In[ ]:
+
+
 exp.data_prepare(target='cnt', task_type='Regression', sample_weight=None,
                  split_method='random', test_ratio=0.2, random_state=0)
 

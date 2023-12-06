659253a0fb047ea16de27f187cd15fd4f17969d5
Zebin YANG <yangzb2010@connect.hku.hk>
2023-12-06 12:07:29 +0800

    update example notebooks

--- docs/_build/jupyter_execute/guides/explain/PDP.py
+++ docs/_build/jupyter_execute/guides/explain/PDP.py
@@ -19,5 +19,5 @@ exp.model_explain(model="XGB2", show="pdp", uni_feature="season",
 
 
 exp.model_explain(model="XGB2", show="pdp", bi_features=["hr", "workingday"],
-                  grid_size=10, pdp_size=10000, sliced_line=False, original_scale=True, figsize=(5, 4))
+                  grid_size=10, sample_size=10000, sliced_line=False, original_scale=True, figsize=(5, 4))
 

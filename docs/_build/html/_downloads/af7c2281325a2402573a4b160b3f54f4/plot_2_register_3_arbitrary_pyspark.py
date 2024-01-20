914202dad9ee9ca700083878c635a3b62bc3fc0e
Zebin YANG <yangzb2010@connect.hku.hk>
2024-01-20 13:52:32 +0800

    update spark example

--- docs/_build/html/_downloads/af7c2281325a2402573a4b160b3f54f4/plot_2_register_3_arbitrary_pyspark.py
+++ docs/_build/html/_downloads/af7c2281325a2402573a4b160b3f54f4/plot_2_register_3_arbitrary_pyspark.py
@@ -14,7 +14,7 @@ import pandas as pd
 from pyspark.ml import Pipeline
 from pyspark.sql import SparkSession
 from pyspark.ml.classification import DecisionTreeClassifier
-from pyspark.ml.feature import VectorAssembler, StringIndexer
+from pyspark.ml.feature import VectorAssembler
 
 data = pd.read_csv('https://github.com/SelfExplainML/PiML-Toolbox/blob/main/datasets/SimuCredit.csv?raw=true')
 feature_names = ['Mortgage', 'Balance', 'Amount Past Due', 'Credit Inquiry', 'Open Trade', 'Delinquency', 'Utilization']
@@ -22,30 +22,26 @@ target_name = 'Approved'
 
 spark = SparkSession.builder.appName("SimuCredit-Spark-Demo").getOrCreate()
 spark_df = spark.createDataFrame(data)
+train_data, test_data = spark_df.randomSplit([0.8, 0.2], seed=2024)
 
 feature_assembler = VectorAssembler(inputCols=feature_names, outputCol="features")
-label_stringIdx = StringIndexer(inputCol=target_name, outputCol='label')
-
-pipeline = Pipeline(stages=[feature_assembler, label_stringIdx])
-pipelineModel = pipeline.fit(spark_df)
-train_data, test_data = pipelineModel.transform(spark_df).randomSplit([0.8, 0.2], seed=2024)
-
-dt = DecisionTreeClassifier(featuresCol='features', labelCol='label', seed=2024)
-model = dt.fit(train_data)
+dt = DecisionTreeClassifier(labelCol=target_name, seed=2024)
+pipeline = Pipeline(stages=[feature_assembler, dt])
+model = pipeline.fit(train_data)
 
 #%%
 # Next, we define the wrapper functions of predict and predict_proba.
 
 def predict_func(X):
 
-    spark_df = pipelineModel.transform(spark.createDataFrame(pd.DataFrame(X, columns=feature_names)))
-    pred = model.transform(spark_df).select('prediction').toPandas().values.astype(float).ravel()
+    sdf = spark.createDataFrame(pd.DataFrame(X, columns=feature_names))
+    pred = model.transform(sdf).select('prediction').toPandas().values.astype(float).ravel()
     return pred
 
 def predict_proba_func(X):
     
-    spark_df = pipelineModel.transform(spark.createDataFrame(pd.DataFrame(X, columns=feature_names)))
-    predictions = model.transform(spark_df).select('probability').toPandas()
+    sdf = spark.createDataFrame(pd.DataFrame(X, columns=feature_names))
+    predictions = model.transform(sdf).select('probability').toPandas()
     proba = predictions.explode('probability').values.reshape((-1, 2)).astype(float)
     return proba
 
@@ -62,12 +58,12 @@ pipeline = exp.make_pipeline(predict_func=predict_func,
                              test_y=test_data.select(target_name).toPandas().values,
                              feature_names=feature_names,
                              target_name=target_name)
-exp.register(pipeline, "Spark-GBT")
+exp.register(pipeline, "Spark-DT")
 
 #%%
 # Check model performance
-exp.model_diagnose(model="Spark-GBT", show="accuracy_table")
+exp.model_diagnose(model="Spark-DT", show="accuracy_table")
 
 #%%
 # Run validataion tests
-exp.model_explain(model="Spark-GBT", show="pdp", uni_feature="Balance", sample_size=1000, figsize=(5, 4))
+exp.model_explain(model="Spark-DT", show="pdp", uni_feature="Balance", sample_size=1000, figsize=(5, 4))

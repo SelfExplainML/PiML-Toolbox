f4347eb56eb525d2416cf77656209103633954e8
Zebin YANG <yangzb2010@connect.hku.hk>
2024-01-22 11:40:18 +0800

    update spark example

--- docs/_build/html/_downloads/af7c2281325a2402573a4b160b3f54f4/plot_2_register_3_arbitrary_pyspark.py
+++ docs/_build/html/_downloads/af7c2281325a2402573a4b160b3f54f4/plot_2_register_3_arbitrary_pyspark.py
@@ -6,14 +6,14 @@ Here we show how to write a wrapper for PySpark models.
 """
 
 #%%
-# For demonstration, we first fit a Decision tree model using SimuCredit data.
+# For demonstration, we first fit a GBT model using SimuCredit data.
 import os
 os.environ["PYSPARK_PYTHON"] = "python"
 
 import pandas as pd
 from pyspark.ml import Pipeline
 from pyspark.sql import SparkSession
-from pyspark.ml.classification import DecisionTreeClassifier
+from pyspark.ml.classification import GBTClassifier
 from pyspark.ml.feature import VectorAssembler
 
 data = pd.read_csv('https://github.com/SelfExplainML/PiML-Toolbox/blob/main/datasets/SimuCredit.csv?raw=true')
@@ -25,8 +25,8 @@ spark_df = spark.createDataFrame(data)
 train_data, test_data = spark_df.randomSplit([0.8, 0.2], seed=2024)
 
 feature_assembler = VectorAssembler(inputCols=feature_names, outputCol="features")
-dt = DecisionTreeClassifier(labelCol=target_name, seed=2024)
-pipeline = Pipeline(stages=[feature_assembler, dt])
+gbt = GBTClassifier(labelCol=target_name, seed=2024)
+pipeline = Pipeline(stages=[feature_assembler, gbt])
 model = pipeline.fit(train_data)
 
 #%%
@@ -58,12 +58,12 @@ pipeline = exp.make_pipeline(predict_func=predict_func,
                              test_y=test_data.select(target_name).toPandas().values,
                              feature_names=feature_names,
                              target_name=target_name)
-exp.register(pipeline, "Spark-DT")
+exp.register(pipeline, "Spark-GBT")
 
 #%%
 # Check model performance
-exp.model_diagnose(model="Spark-DT", show="accuracy_table")
+exp.model_diagnose(model="Spark-GBT", show="accuracy_table")
 
 #%%
 # Run validataion tests
-exp.model_explain(model="Spark-DT", show="pdp", uni_feature="Balance", sample_size=1000, figsize=(5, 4))
+exp.model_explain(model="Spark-GBT", show="pdp", uni_feature="Balance", sample_size=1000, figsize=(5, 4))
